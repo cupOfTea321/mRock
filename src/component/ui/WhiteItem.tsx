cc420bc6147fbb621659e1a5adb98b19609d274f
Alex <ddragon99999@gmail.com>
2024-02-20 09:16:17 +0300

    button hover

--- src/component/ui/WhiteItem.tsx
+++ src/component/ui/WhiteItem.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import whiteBack from "../../assets/items/tickets.webp";
+import greenHover from "../../assets/rightHover.svg";
 import whiteBackM from "../../assets/items/ticketsM.webp";
 import {Box, Typography} from "@mui/material";
 import {backText, h3} from "../../mui/palette";
+import ym from "react-yandex-metrika";
+import {motion} from "framer-motion";
 
 const WhiteItem =  ({sx}) => {
+    const yandexClick = () => {
+        console.log('yandexClick')
+        ym(96408862, 'reachGoal', 'tickets')
+    }
     return (
-        <Box  sx={{
+        <Box component={motion.div}  sx={{
             ...backText,
             cursor: 'pointer',
             width: {sm: '200px', xs: '115px'},
@@ -21,7 +28,13 @@ const WhiteItem =  ({sx}) => {
             fontWeight: 400,
             display: 'flex',
             alignItems: 'center',
+            // backgroundBlendMode: 'multiply',
+            // transition: 'background-color 0.3s ease',
             ...sx
+        }} onClick={yandexClick}
+             whileHover={{
+                 backgroundImage: `url(${greenHover})`,
+                 transition: { delay: 0.1, duration: 0.2 }
         }}
               >
             {/*<Typography sx={h3}>*/}
