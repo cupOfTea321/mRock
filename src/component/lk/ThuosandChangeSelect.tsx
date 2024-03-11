5ee4fccbacc09219fb6d057cbad0e5dcb87786c4
Evgenii Simanin <evgenijsimanin74@gmail.com>
2024-03-11 10:15:46 +0300

    Fix select on Thuosand Page

---
+++ src/component/lk/ThuosandChangeSelect.tsx
@@ -0,0 +1,47 @@
+import { rolesTarget } from "../../handlers/rolesTarget";
+import { Box, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+
+const roles = ["Гитарист", "Вокалист", "Барабанщик", "Басист"];
+
+const ThousandChangeSelect = ({
+   inputStyle,
+   basicInput,
+   formState,
+   handleChange,
+}: any) => {
+   return (
+      <div className={"custom-select"}>
+         <Box
+            sx={{
+               ...inputStyle,
+               ...basicInput,
+
+               "&::-ms-expand": {
+                  color: "black",
+                  background: "red",
+               },
+            }}
+            component={"select"}
+            name={"role"}
+            id="roleSelect"
+            value={formState?.role}
+            onChange={handleChange}>
+            <option
+               style={{ color: "black" }}
+               value="Роль"
+               disabled
+               selected
+               hidden>
+               Роль
+            </option>
+            {roles.map((item) => (
+               <option style={{ color: "black" }} value={item}>
+                  {item}
+               </option>
+            ))}
+         </Box>
+      </div>
+   );
+};
+
+export default ThousandChangeSelect;
--- src/component/thousand/CardsBlock.tsx
+++ src/component/thousand/CardsBlock.tsx
@@ -1,116 +1,115 @@
-import {Box, Container, Grid} from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import moreBtn from "../../assets/hovers/purpleHover.webp";
 import person1 from "../../assets/thousand/img1.png";
 import person2 from "../../assets/thousand/img2.png";
 import person3 from "../../assets/thousand/img3.png";
 import person4 from "../../assets/thousand/img4.png";
-import {input, Input} from "../ui/Input";
+import { input, Input } from "../ui/Input";
 import ItemText from "../ui/ItemText";
 import PeopleCard from "./PeopleCard";
 import ChangeSelect from "../lk/ChangeSelect";
 import React from "react";
+import ThousandChangeSelect from "../lk/ThuosandChangeSelect";
 
 const CardsBlock = ({ data }) => {
-  const people = [
-    { image: person1 },
-    { image: person2 },
-    { image: person3 },
-    { image: person4 },
-  ];
-  return (
-    <Container
-      sx={{
-        zIndex: 600,
-        position: "relative",
-        // display: 'flex',
-        marginBottom: "80px",
-      }}
-    >
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          // justifyContent: 'center',
-        }}
-        className={"animate__fadeInUp animate__animated wow"}
-      >
-        <Input sx={{
-            label: {
-                color: 'white',
-                paddingLeft: "16px",
-            },
-
-        }} title={"Имя"} placehold={""} />
-          {/*ТУТ !!!!!!!!!!!!!!!!!*/}
-          <ChangeSelect
-              inputStyle={{
-                  ...input,
-                  color: 'white',
-
-                  width: {md: "216px", xs: "300px"},
-                  height: {md: "60px", xs: "50px"},
-                  label: {
-                      color: 'black',
-                  },
-                  background: 'transparent',
-                  border: '2px solid #8654CC',
-                  borderRadius: '100px',
-                  // marginBottom: '16px',
-                  paddingLeft: '16px',
-                  ':hover': {
-                      color: 'white !important'
-                  },
-              }}
-              // basicInput={authText}
-              // formState={formState}
-              // handleChange={handleChange}
-              // roles={roles}
-          />
-        {/*<MyAuto   sxAuto={{*/}
-        {/*    width: "216px",*/}
-        {/*    color: 'white',*/}
-        {/*    label: {*/}
-        {/*        color: 'white',*/}
-        {/*        paddingLeft: "16px",*/}
-        {/*    },*/}
-        {/*}}/>*/}
-      </Box>
-      <Grid
-        container
-        sx={{
-          marginTop: "72px",
-        }}
-      >
-        {data?.map((item, index) => (
-          <Grid
-            key={data.name}
-            item
-            lg={3}
-            md={3}
-            sm={6}
-            xs={6}
+   const people = [
+      { image: person1 },
+      { image: person2 },
+      { image: person3 },
+      { image: person4 },
+   ];
+   return (
+      <Container
+         sx={{
+            zIndex: 600,
+            position: "relative",
+            // display: 'flex',
+            marginBottom: "80px",
+         }}>
+         <Box
             sx={{
-              display: "flex",
-              justifyContent: "center",
+               display: "flex",
+               alignItems: "center",
+               // justifyContent: 'center',
             }}
-          >
-            <PeopleCard person={item} />
-          </Grid>
-        ))}
-      </Grid>
-      <ItemText
-        img={moreBtn}
-        title={"Загрузить ещё"}
-        typo={{ color: "white" }}
-        sx={{
-          margin: "0 auto",
-          width: { sm: "190px", xs: "189px" },
-          height: { sm: "43px", xs: "42px" },
-          marginTop: { sm: 0, xs: "40px" },
-        }}
-      />
-    </Container>
-  );
+            className={"animate__fadeInUp animate__animated wow"}>
+            <Input
+               sx={{
+                  label: {
+                     color: "white",
+                     paddingLeft: "16px",
+                  },
+               }}
+               title={"Имя"}
+               placehold={""}
+            />
+            <ThousandChangeSelect
+               inputStyle={{
+                  ...input,
+                  color: "white",
+
+                  width: { md: "216px", xs: "300px" },
+                  height: { md: "60px", xs: "50px" },
+                  label: {
+                     color: "black",
+                  },
+                  background: "transparent",
+                  border: "2px solid #8654CC",
+                  borderRadius: "100px",
+                  // marginBottom: '16px',
+                  paddingLeft: "16px",
+                  ":hover": {
+                     color: "white !important",
+                  },
+               }}
+               // basicInput={authText}
+               // formState={formState}
+               // handleChange={handleChange}
+               // roles={roles}
+            />
+            {/*<MyAuto   sxAuto={{*/}
+            {/*    width: "216px",*/}
+            {/*    color: 'white',*/}
+            {/*    label: {*/}
+            {/*        color: 'white',*/}
+            {/*        paddingLeft: "16px",*/}
+            {/*    },*/}
+            {/*}}/>*/}
+         </Box>
+         <Grid
+            container
+            sx={{
+               marginTop: "72px",
+            }}>
+            {data?.map((item, index) => (
+               <Grid
+                  key={data.name}
+                  item
+                  lg={3}
+                  md={3}
+                  sm={6}
+                  xs={6}
+                  sx={{
+                     display: "flex",
+                     justifyContent: "center",
+                  }}>
+                  <PeopleCard person={item} />
+               </Grid>
+            ))}
+         </Grid>
+         <ItemText
+            img={moreBtn}
+            title={"Загрузить ещё"}
+            typo={{ color: "white" }}
+            sx={{
+               margin: "0 auto",
+               width: { sm: "190px", xs: "189px" },
+               height: { sm: "43px", xs: "42px" },
+               marginTop: { sm: 0, xs: "40px" },
+            }}
+         />
+      </Container>
+   );
 };
 
 export default CardsBlock;
