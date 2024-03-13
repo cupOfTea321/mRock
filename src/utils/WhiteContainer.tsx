05a760607eafe91ed76e5667ae9789460eca154c
Evgenii Simanin <evgenijsimanin74@gmail.com>
2024-03-13 11:52:49 +0300

    Fix partners

--- src/utils/WhiteContainer.tsx
+++ src/utils/WhiteContainer.tsx
@@ -2,33 +2,32 @@ import { Box } from "@mui/material";
 import back from "../assets/Union.png";
 
 const WhiteContainer = ({ children, sx }) => {
-  return (
-    <Box
-      sx={{
-        width: "100vw",
-        marginTop: "-50px",
-        marginBottom: "-50px",
-        paddingBottom: "120px",
-        zIndex: 350,
-        height: "50%",
-        position: "relative",
-        ...sx,
-      }}
-    >
+   return (
       <Box
-        component={"img"}
-        width={"100%"}
-        src={back}
-        sx={{
-          position: "absolute",
-          height: "100%",
-          bottom: 0,
-          zIndex: -100,
-        }}
-      />
-      {children}
-    </Box>
-  );
+         sx={{
+            width: "100vw",
+            marginTop: "-50px",
+            marginBottom: "-50px",
+            paddingBottom: "120px",
+            zIndex: 400,
+            height: "50%",
+            position: "relative",
+            ...sx,
+         }}>
+         <Box
+            component={"img"}
+            width={"100%"}
+            src={back}
+            sx={{
+               position: "absolute",
+               height: "100%",
+               bottom: 0,
+               zIndex: -100,
+            }}
+         />
+         {children}
+      </Box>
+   );
 };
 
 export default WhiteContainer;
